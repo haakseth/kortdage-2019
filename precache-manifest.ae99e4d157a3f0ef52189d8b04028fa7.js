29b5d0659f3e727b45a3b1dd6c4203e3a8aeb055
John Wika Haakseth <johnwika@gmail.com>
2019-11-04 16:32:03 +0100

    Updates

---
+++ precache-manifest.ae99e4d157a3f0ef52189d8b04028fa7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "104bc75ad879af237189fa00a03db823",
+    "url": "/kortdage-2019/static/media/waldemar-brandt-U3Ptj3jafX8-unsplash.104bc75a.jpg"
+  },
+  {
+    "revision": "0b72396c208179a72b36",
+    "url": "/kortdage-2019/static/css/main.e90a68ac.chunk.css"
+  },
+  {
+    "revision": "a3d7733bff5fd988f4bf327268ca7f8c",
+    "url": "/kortdage-2019/static/media/shaka2.a3d7733b.jpg"
+  },
+  {
+    "revision": "592ffe2090e541cd2247",
+    "url": "/kortdage-2019/static/js/1.592ffe20.chunk.js"
+  },
+  {
+    "revision": "2c043c7b95dfbdc50427",
+    "url": "/kortdage-2019/static/js/runtime~main.2c043c7b.js"
+  },
+  {
+    "revision": "0b72396c208179a72b36",
+    "url": "/kortdage-2019/static/js/main.0b72396c.chunk.js"
+  },
+  {
+    "revision": "95cbd538064400f7c358ee676e878997",
+    "url": "/kortdage-2019/static/media/thomas-kelley-5YtjgRNTli4-unsplash.95cbd538.jpg"
+  },
+  {
+    "revision": "7fa6fe8c1a4068b978612eb893aa6c3e",
+    "url": "/kortdage-2019/static/media/andrew-neel-1-29wyvvLJA-unsplash.7fa6fe8c.jpg"
+  },
+  {
+    "revision": "8c6c282a11063e8efd5a5525ee4101b4",
+    "url": "/kortdage-2019/static/media/shaka1.8c6c282a.jpg"
+  },
+  {
+    "revision": "592ffe2090e541cd2247",
+    "url": "/kortdage-2019/static/css/1.f23a2e37.chunk.css"
+  },
+  {
+    "revision": "58545bb787700429ffa1d356e008125a",
+    "url": "/kortdage-2019/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/kortdage-2019/precache-manifest.ae99e4d157a3f0ef52189d8b04028fa7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/kortdage-2019/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
